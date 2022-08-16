d510732c56f7a1278fbdc2be94acdfd8de4e2aa1
NOT (Henrique) <110069021+NOT39@users.noreply.github.com>
2022-08-15 22:10:32 -0300

    Adicionei alguns comentários e explicações

--- script.py
+++ script.py
@@ -2,7 +2,18 @@ import os
 
 
 def renomear(path, output, name):
+    '''
+    Recebe um caminho, saída e nome, renomeia os arquivos .srt do caminho e envia pra saída.
+    :param path: Endereço da pasta com os arquivos .srt
+    :param output: Endereço de saída dos arquivos
+    :param name: Nome que os arquivos serão renomeados
+    :return: 
+    '''
+
+    #Faz uma lista com o nome de todos os arquivos no caminho de origem.
     a = os.listdir(path)
+
+    #Verifica se o arquivo possuí a extensão .srt e os renomeia.
     for c in a:
         if '.srt' in c:
             n = c[:2]
