a8e74c3f13c30a6404e00c0252f4e0518cff7076
NOT (Henrique) <110069021+NOT39@users.noreply.github.com>
2022-08-15 22:01:10 -0300

    Arrumando mais alguns erros de digitação

--- script.py
+++ script.py
@@ -1,16 +1,16 @@
 import os
 
 
-def renomear(path):
+def renomear(path, output, name):
     a = os.listdir(path)
     for c in a:
         if '.srt' in c:
             n = c[:2]
             os.rename(f'{path}/{c}',
-                    f'{saida}/{nome.replace("E00", f"E{n}")}.srt')
+                    f'{output}/{name.replace("E00", f"E{n}")}.srt')
 
 
 endereco = str(input('Insira o endereço dos arquivos: '))
 saida = str(input('Digite o endereço de saída: '))
 nome = str(input('Digite o novo nome do arquivo com E00 no lugar do episódio: '))
-renomear(endereco)
+renomear(endereco, saida, nome)
